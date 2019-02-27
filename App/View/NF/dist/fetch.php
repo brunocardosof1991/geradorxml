6084c0533c073c47cd65d27d8bf807b8db6a085a
Bruno <bruno.bitbucket@gmail.com>
2019-02-27 10:19:22 -0300

    View/NF with parcel-bundler

--- App/View/NF/dist/fetch.php
+++ App/View/NF/dist/fetch.php
@@ -10,12 +10,34 @@ if(!isset($_SESSION['usuario'])){
         <!-- ======================== META TAGS OBRIGATÓRIAS ================================================== -->
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">       
+
+        <!-- JQUERY-->
+        <script src="../../../js/external/jquery/jquery.js"></script>       
+
+        <!-- CustomJS -->
+        <script type="module" src="../../../js/NF/index.js"></script>
+        <script type="text/javascript" src="../../../js/external/tableResponsive.js"></script>
+        <script src="../../../js/logout.js"></script>
+        <!-- QRCode -->
+        <script type="text/javascript" src="../../../js/external/jquery/jquery.qrcode.js"></script>
+        <script type="text/javascript" src="../../../js/external/qrcode.js"></script>
+        <!-- DataTable-->   
+        <script src="../../../js/external/datatables.js"></script> 
         <link href="../../../css/datatables.css" type="text/css" rel="stylesheet"> 
         <!-- Folha de estilo da página -->
         <link href="../../../css/style.css" type="text/css" rel="stylesheet"> 
         <!-- fonte awesome -->
         <link href="../../../fontawesome-free-5.6.3/css/all.css" rel="stylesheet">
+        <!-- Bootstrap-->
+        <script src="../../../js/bootstrap/bootstrap.min.js"></script>
         <link rel="stylesheet" href="../../../css/bootstrap/bootstrap.min.css">
+        <!-- Barra de progressp -->
+        <script src="../../../easy-pie-chart-master/dist/jquery.easypiechart.js"></script>
+        <script>
+            $(function () {
+                $('[data-toggle="tooltip"]').tooltip();
+            });
+        </script>        
     </head>
     <body>     
     <?php require_once '../Menu.php'; ?> 
@@ -37,29 +59,14 @@ if(!isset($_SESSION['usuario'])){
                             <tbody></tbody>
                         </table>
                     </div> <!-- END #table-responsive -->
+                <div class="col-md-12" id="colInutilizar">
+                </div>
                 </div>
             </div> <!-- END .row -->
         </div> <!-- END .container --> 
         <?php require_once '../../Components/Modal/Modal.html'; ?> 
         <?php require_once '../../Components/Modal/ModalQRCode.html'; ?> 
-
-        <!-- JQUERY-->
-        <script src="../../../js/external/jquery/jquery.js"></script>     
-        <script src="../../../js/external/jquery/jquery-ui.js"></script>     
-        <!-- CustomJS -->
-        <script type="module" src="../../../js/NF/index.js"></script>
-        <script type="text/javascript" src="../../../js/external/tableResponsive.js"></script>
-        <script src="../../../js/logout.js"></script>
-        <!-- QRCode -->
-        <script type="text/javascript" src="../../../js/external/jquery/jquery.qrcode.js"></script>
-        <script type="text/javascript" src="../../../js/external/qrcode.js"></script>
-        <!-- DataTable-->   
-        <script src="../../../js/external/datatables.js"></script> 
-        <!-- Bootstrap-->
-        <script src="../../../js/bootstrap/bootstrap.min.js"></script> 
-        <!-- Barra de progresso -->
-        <script src="../../../easy-pie-chart-master/dist/jquery.easypiechart.js"></script>
-               
+        <?php require_once '../../Components/FormInutilizar.html'; ?>         
         <?php require_once '../../View/Footer.php'; ?>
     </body>
 </html>
